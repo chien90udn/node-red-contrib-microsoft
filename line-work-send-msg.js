df3c983c5c92e1577f807915d33369ed4efe4a46
ChienNguyen <chienduc@mor.com.vn>
2021-10-29 08:42:08 +0700

    update

--- line-work-send-msg.js
+++ line-work-send-msg.js
@@ -6,7 +6,8 @@ module.exports = function (RED) {
       let roomId = config.roomid;
       let apiId = config.apiid;
       let botId = config.botid;
-      let message = config.message;
+      let message = msg.payload;
+      let accountId = config.accountid;
       let consumerKey = config.consumerkey;
       let token = config.token;
       let url = `https://apis.worksmobile.com/r/${apiId}/message/v1/bot/${botId}/message/push`;
@@ -16,13 +17,25 @@ module.exports = function (RED) {
         Authorization: `Bearer ${token}`,
         'Content-Type': 'application/json'
       };
-      msg.payload = {
-        roomId,
-        "content": {
-          "type": "text",
-          "text": message
-        }
-      };
+      // check position send message
+      // accountId prioritized
+      if(accountId){
+        msg.payload = {
+          accountId,
+          "content": {
+            "type": "text",
+            "text": message
+          }
+        };
+      }else{
+        msg.payload = {
+          roomId,
+          "content": {
+            "type": "text",
+            "text": message
+          }
+        };
+      }
       node.send(msg);
     });
   }
